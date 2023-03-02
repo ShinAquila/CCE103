da8692ca8bc714f6756ac3c46d602740b00d0d24
ShinAquila <augusto08rivera12@gmail.com>
2023-03-02 18:23:09 +0800

    init

--- StringManipulation/MidName.java
+++ StringManipulation/MidName.java
@@ -6,28 +6,32 @@ public class MidName {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
-        System.out.println("[!] Please put a '-' at the start of your Middle Name [!]\n");
-        System.out.print("Enter your name: ");
-        String nameInput = scan.nextLine();
+        try {
+            System.out.println("[!] Please put a '-' at the start of your Middle Name [!]\n");
+            System.out.print("Enter your name: ");
+            String nameInput = scan.nextLine();
+            
+            String arrName[] = nameInput.split("[ ]");
 
-        String arrName[] = nameInput.split("[ ]");
-
-        System.out.println();
-        for (int i = 0; i < arrName.length; i++) {
-            if (arrName[i].charAt(0) == '-') {
-                System.out.print(arrName[i].charAt(1) + ". ");
-            } else {
-                System.out.print(arrName[i] + " ");
+            System.out.println();
+            for (int i = 0; i < arrName.length; i++) {
+                if (arrName[i].charAt(0) == '-') {
+                    System.out.print(arrName[i].charAt(1) + ". ");
+                } else {
+                    System.out.print(arrName[i] + " ");
+                }
             }
-        }
-        System.out.println();
+            System.out.println();
 
-        for (int i = 0; i < arrName.length; i++) {
-            if (arrName[i].charAt(0) == '-') {
-                System.out.print(arrName[i].charAt(1));
-            } else {
-                System.out.print(arrName[i].charAt(0));
+            for (int i = 0; i < arrName.length; i++) {
+                if (arrName[i].charAt(0) == '-') {
+                    System.out.print(arrName[i].charAt(1));
+                } else {
+                    System.out.print(arrName[i].charAt(0));
+                }
             }
+        } catch (Exception e) {
+            System.out.println("An error has occurred.");
         }
 
         System.out.println();
