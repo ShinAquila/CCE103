f1c3279cdaec39d569a73abb99e51f8d540624a4
ShinAquila <augusto08rivera12@gmail.com>
2023-03-05 16:38:51 +0800

    init

---
+++ FinalProject/Exercises5/Exer1Anagram.java
@@ -0,0 +1,89 @@
+package Exercises5;
+import java.util.*;
+
+public class Exer1Anagram {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String input[] = new String[2];
+
+        try {
+            do {
+                System.out.println("-".repeat(12)+ "<[ INPUT ]>" +"-".repeat(12)+"\n");
+                for (int i = 0; i < input.length; i++) {
+                    System.out.print("[ "+ (i+1) +" ] Enter any number/word: ");
+                    input[i] = scan.nextLine(); 
+                }
+
+                if (input[0].length()>20) {
+                    System.out.println("\nInput 1 Exceeded 20 characters");
+                }
+                if (input[1].length()>20) {
+                    System.out.println("\nInput 2 Exceeded 20 characters");
+                }
+                System.out.println();
+            } while (input[0].length()>20 || input[1].length()>20);
+
+            String word1 = input[0].toLowerCase().replaceAll("[\\s+ \\W+]", "");
+            String word2 = input[1].toLowerCase().replaceAll("[\\s+ \\W+]", "");
+            
+            
+            boolean sureNotAnagram = false;
+            int count1 [] = new int[word1.length()];
+            int count2 [] = new int[word1.length()];
+            
+            boolean anagramCheck[] = new boolean[word1.length()];
+            for (int i = 0; i < word1.length(); i++) {
+                for (int j = 0; j < word2.length(); j++) {
+                    if (word1.charAt(i) == word2.charAt(j)) {
+                        anagramCheck[i] = true;
+                        break;
+                    } else {
+                        anagramCheck[i] = false;
+                    }
+                }
+
+                try {
+                    count1[word1.indexOf(word1.charAt(i))]++;
+                    count2[word1.indexOf(word2.charAt(i))]++;
+                    sureNotAnagram = false;
+                } catch (ArrayIndexOutOfBoundsException ArrEx) {
+                    sureNotAnagram = true;
+                }
+            }
+            
+            boolean sameCount = true;
+            if (sureNotAnagram == false) {
+                for (int i = 0; i < count1.length; i++) {
+                    if (count1[i] == count2[i]) {
+                        sameCount = true;
+                    } else {
+                        sameCount = false;
+                        break;
+                    }
+                }
+            }
+            
+            boolean isAnagram = true;
+            for (int i = 0; i < anagramCheck.length; i++) {
+                if (anagramCheck[i] == true && sameCount == true && (word1.length() == word2.length())) {
+                    isAnagram = true;
+                } else {
+                    isAnagram = false;
+                    break;
+                }
+            }
+
+            System.out.println("-".repeat(12)+ "<[ OUTPUT ]>" +"-".repeat(12));
+            System.out.print("\n[!] "+input[0] + " and " + input[1] + " are ");
+            if (isAnagram) {
+                System.out.print("Anagrams <OK>\n");
+            } else if (isAnagram == false) {
+                System.out.print("NOT Anagrams <XX>\n");
+            }
+        } catch (Exception e) {
+            System.out.println("An Error has occurred...");
+        }
+        
+        scan.close();
+    }
+}
---
+++ FinalProject/Exercises5/Exer5Palindrome.java
@@ -0,0 +1,29 @@
+package Exercises5;
+import java.util.*;
+
+public class Exer5Palindrome {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("-".repeat(12)+ "<[ INPUT ]>" +"-".repeat(12)+"\n");
+        System.out.print("Enter a word: ");
+        String wordInput = scan.nextLine();
+
+        String word = wordInput.toLowerCase().replaceAll("\\s+", "");
+
+        StringBuilder revWordTemp = new StringBuilder();
+        for (int i = word.length()-1; i >= 0 ; i--) {
+            revWordTemp.append(word.charAt(i));
+        }
+        String revWord = revWordTemp.toString();
+
+        System.out.println("\n"+"-".repeat(12)+ "<[ OUTPUT ]>" +"-".repeat(12)+"\n");
+        if (revWord.equalsIgnoreCase(word)) {
+            System.out.println(wordInput + " is a palindrome");
+        } else {
+            System.out.println(wordInput + " is NOT a palindrome");
+        }
+
+        scan.close();
+    }
+}
